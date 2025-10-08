cbb5b5c385eacbc99232f9f64a04537e2b671b3b
Isabela Damasceno <161511929+isabeladdamasceno@users.noreply.github.com>
2025-10-08 14:22:46 -0300

    alteracoes
    alterações

--- FRONT-END/src/index.js
+++ FRONT-END/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './style/global.css';  // ← importe os estilos aqui
-import './style/login.css';   // ← importe os estilos aqui
+import './style/global.css';  
+import './style/login.css';   
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
